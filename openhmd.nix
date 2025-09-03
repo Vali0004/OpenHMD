fd1a8866dc10540bcfe2938984c1c5cb932527fa
Vali0004 <diorcheats.vali@gmail.com>
2025-09-03 10:51:36 -0400

    Change openhmd

--- openhmd.nix
+++ openhmd.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation {
   name = "OpenHMD";
   allowSubstitutes = false;
   src = ./.;
-  nativeBuildInputs = [ cmake pkg-config ninja ];
+  nativeBuildInputs = [ cmake pkg-config ];
 
   buildInputs = [
     hidapi
@@ -28,13 +28,20 @@ stdenv.mkDerivation {
     libGL
   ];
 
-  installPhase = ''
-    mkdir -p $out/lib
-    cp libopenhmd.a $out/lib
-    ${lib.optionalString withExamples ''
-      mkdir -p $out/bin
-      cp examples/simple/simple $out/bin
-      cp examples/opengl/openglexample $out/bin
-    ''}
+
+  cmakeFlags = [
+    "-DBUILD_BOTH_STATIC_SHARED_LIBS=ON"
+    "-DOPENHMD_EXAMPLE_SIMPLE=${examplesOnOff}"
+    "-DOPENHMD_EXAMPLE_SDL=${examplesOnOff}"
+    "-DOpenGL_GL_PREFERENCE=GLVND"
+
+    # RPATH of binary /nix/store/.../bin/... contains a forbidden reference to /build/
+    "-DCMAKE_SKIP_BUILD_RPATH=ON"
+  ];
+
+  postInstall = lib.optionalString withExamples ''
+    mkdir -p $out/bin
+    install -D examples/simple/simple $out/bin/openhmd-example-simple
+    install -D examples/opengl/openglexample $out/bin/openhmd-example-opengl
   '';
 }
